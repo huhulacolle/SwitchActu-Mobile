e7b0fc946a912c633834231fbb003fa38e5d793a
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-11-09 09:37:29 +0100

    c'est dégueulasse mais sa marche

--- pages/Articles.tsx
+++ pages/Articles.tsx
@@ -1,14 +1,13 @@
-import { Button, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { WP_REST_API_Posts } from 'wp-types';
 import { Card } from 'react-native-paper';
 import dayjs from 'dayjs';
-import CardContent from 'react-native-paper/lib/typescript/components/Card/CardContent';
 
 export default function Articles({navigation}: any) {
 
-  const [Articles, setArticles] = useState<WP_REST_API_Posts>([]);
+  const [Articles, setArticles] = useState<any[]>([]);
 
   useEffect(() => {
     getArticles();
@@ -30,10 +29,11 @@ export default function Articles({navigation}: any) {
 
   return (
     <ScrollView style={style.cardview}>
-      {Articles.map(article => {
+      {Articles.map((article: { id: React.Key | null | undefined; yoast_head_json: { og_image: { url: any; }[]; }; title: { rendered: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }; date: string | number | Date | dayjs.Dayjs | null | undefined; }) => {
         return(
           <View key={article.id} style={{marginTop: 10}}>
             <Card onPress={() => navigation.navigate("Article", article)}>
+              <Card.Cover source={{uri: article.yoast_head_json.og_image[0].url}} />
               <Card.Title
               title={article.title.rendered}
               subtitle={dayjs(article.date).format('DD/MM/YYYY')}
